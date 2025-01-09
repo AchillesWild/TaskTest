55e5c5963724c2b29b1abe91ce302e0118d0d785
AchillesWild <56060020+AchillesWild@users.noreply.github.com>
2025-01-09 18:15:30 +0800

    task

--- src/test/java/com/test/BaseTest.java
+++ src/test/java/com/test/BaseTest.java
@@ -1,12 +1,12 @@
 package com.test;
 
-import com.achilles.SpringBootModelApplication;
+import com.achilles.TaskTestApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes= SpringBootModelApplication.class)
+@SpringBootTest(classes= TaskTestApplication.class)
 public class BaseTest {
 
 }
